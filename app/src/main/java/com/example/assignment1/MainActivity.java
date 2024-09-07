bbbc8b1f541d195dc6fb3abe096b9c3ae7694054
samiyatahersumaiya <samiyatahersumaiya@gmail.com>
2024-09-07 23:17:17 +0600

    Final Commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assignment1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change text in TextView
+                textView.setText("Button Clicked!");
+
+                // Show a Toast message
+                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
